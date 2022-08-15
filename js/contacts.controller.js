7f423180837ad44d1e5d95d8718f59bd7002266f
Gilad Peled <Gilad>
2022-08-15 20:22:14 +0200

    added pictures

--- js/contacts.controller.js
+++ js/contacts.controller.js
@@ -9,7 +9,7 @@ function renderContacts(contacts) {
 
     contacts.map((contact) => {
         contact = `<div class="grid-item"><h4 class="header name">${contact.lname}, ${contact.fname}</h4>
-        <div class="content-container flex"><img src="" alt="">
+        <div class="content-container flex"><img src="https://robohash.org/${contact.fname}?set=set1" alt="">
         <ul><li>Phone:${contact.tel}</li><li>City:${contact.city}</li><li>State:${contact.state}</li><li>Zip:${contact.zip}</li><li>Address:${contact.address}</li></ul></div></div>`
 
         container.innerHTML += contact
